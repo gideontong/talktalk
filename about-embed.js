16399406fe9fc657c0c618aba8dcb4470743ba3d
Gideon Tong <gideon@gideontong.com>
2020-04-26 20:53:35 -0700

    Create about embeds

---
+++ about-embed.js
@@ -0,0 +1,37 @@
+const embed = {
+    "title": "About SMH OUR HEADS",
+    "description": "SMH OUR HEADS is a weekly podcast created and recorded by the youth of ACT4Christ Youth/College Ministries at CCCTO. We want to show how we can love God, to love others, and to make disciples who can continue to do the same.",
+    "url": "https://smhourheads.com",
+    "color": 16776960,
+    "timestamp": "2020-04-01T00:00:00",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/embed/avatars/2.png",
+      "text": "SMH Our Bot ✔"
+    },
+    "thumbnail": {
+      "url": "https://cdn.discordapp.com/icons/594769167169880089/80090605aad15d483b134c9b9e7b0ddc.png"
+    },
+    "image": {
+      "url": "https://smhourheads.com/images/banner_f92jc.png"
+    },
+    "author": {
+      "name": "Gideon Tong",
+      "url": "https://gideontong.com",
+      "icon_url": "https://cdn.discordapp.com/avatars/132525049977503744/cabd6e3685f9fafe3755658750d4e8aa.png"
+    },
+    "fields": [
+      {
+        "name": "❄ Rules ❄",
+        "value": "We love because he first loved us. - *1 John 4:19*"
+      },
+      {
+        "name": "🔥 Admins 🔥",
+        "value": "Need help? Feel free to message anyone with the **Podcast Host** or **Podcaster** role!"
+      },
+      {
+        "name": "🌿 Enjoy! 🌿",
+        "value": "Thanks for coming! We hope you'll listen to our podcasts and stick around to hear about what you have to say. Questions or suggestions? Feel free to provide those as well."
+      }
+    ]
+  };
+  channel.send("Welcome to the **SMH OUR HEADS** Discord server!", { embed });
