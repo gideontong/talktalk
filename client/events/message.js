4d95ab098783b5547d8065163ff08b925f4e3370
Gideon Tong <gideon@gideontong.com>
2020-05-01 18:30:42 -0700

    Fix messages and start calls

--- client/events/message.js
+++ client/events/message.js
@@ -7,11 +7,19 @@ const config = require('../../config/config.json');
 
 module.exports = async msg => {
     if (!msg.content.startsWith(config.commands.prefix) || msg.author.bot) return;
+    const bot = msg.client;
     const args = msg.content.split(` `);
-    const command = args.shift().slice(config.prefix.length);
+    const command = args.shift().slice(config.commands.prefix.length);
     try {
-        command = require(`../../commands/${command}.js`);
+        console.log(`Running ${command}...`);
+        runCommand = require(`../../commands/${command}.js`);
     } catch {
         console.error(`User attempted to execute ${command} but it wasn't found!`);
+    }
+
+    if(runCommand) {
+        runCommand(bot, msg, args).catch(err => {
+            // do something with errors
+        })
     }
 }
--- commands/startcall.js
+++ commands/startcall.js
@@ -3,11 +3,17 @@ const config = require('../config/config.json');
 const dictionary = require('../config/dictionary.json');
 
 module.exports = async(bot, msg, args) => {
-    url = ''.join([config.podcast.video,
-        random.choice(dictionary.adjectives),
-        random.choice(dictionary.bible),
-        "In", random.choice(dictionary.places)]);
+    console.log("Generating a meeting...");
+
+    const adjective = choose(dictionary.adjectives);
+    const bible = choose(dictionary.bible);
+    const place = choose(dictionary.places);
+    const url = config.podcast.video + adjective + bible + "In" + place;
     
     msg.channel.send("Here's your new meeting: " + url);
     console.log("New video meeting generated at " + url);
+}
+
+function choose(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
