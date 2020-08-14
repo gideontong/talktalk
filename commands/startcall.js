6087785c8bc0f47b1cfa9344cb973f2b19d43533
Gideon Tong <gideon@gideontong.com>
2020-08-13 23:07:43 -0700

    Remove logging

--- commands/startcall.js
+++ commands/startcall.js
@@ -2,15 +2,12 @@ const config = require('../config/config.json');
 const dictionary = require('../config/dictionary.json');
 
 module.exports = async(bot, msg, args) => {
-    console.log("Generating a meeting...");
-
     const adjective = choose(dictionary.adjectives);
     const bible = choose(dictionary.bible);
     const place = choose(dictionary.places);
     const url = config.podcast.video + adjective + bible + "In" + place;
     
     msg.channel.send("Here's your new meeting: " + url);
-    console.log("New video meeting generated at " + url);
 }
 
 function choose(arr) {
