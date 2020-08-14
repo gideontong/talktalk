ff4d0ab50001acad5a82a5552c482eb9d4e1b2a7
Gideon Tong <gideon@gideontong.com>
2020-08-13 23:06:13 -0700

    Import scaffolding from `gideontong/Bot4Christ`

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-// Main Runtime for SMH OUR Bot
-// Author: Gideon Tong
-// 2020 May 01
+// Local config files
+const { commands } = require('./config/config.json');
+const { token } = require('./config/secrets.json');
 
+// Dependencies
 const Discord = require('discord.js');
+const log4js = require('log4js');
 const client = new Discord.Client();
 
-const secrets = require("./config/secrets.json");
+function createLogName() {
+    let date = new Date();
+    return date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate() + '.' +
+        date.getHours() + '.' + date.getMinutes() + '.' + date.getSeconds() + '.log';
+}
 
-require('./client/handler.js')(client);
+log4js.configure({
+    appenders: {
+        console: {
+            type: 'console'
+        },
+        file: {
+            type: 'file',
+            filename: `logs/${createLogName()}`
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['console', 'file'],
+            level: 'info'
+        }
+    }
+});
+const log = log4js.getLogger('podcast');
 
-client.once('ready', () => {
-	console.log('Ready!');
+require('./events.js')(client);
+
+client.on('ready', () => {
+	log.info(`The bot has begun startup as ${client.user.tag} on prefix ${commands.prefix}`);
+	client.user.setActivity('your next podcast', { type: 'LISTENING' })
 });
 
-client.login(secrets.token);
+client.login(token);
